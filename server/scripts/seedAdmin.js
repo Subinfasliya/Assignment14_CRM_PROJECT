e67018d46bf0fa8dee4badeaefce48cf853b02b5
Subin <subinfasliya@gmail.com>
2026-09-24 15:20:34 +0530

    feat: Update admin seeding logic to use environment variables for admin details

--- server/scripts/seedAdmin.js
+++ server/scripts/seedAdmin.js
@@ -8,37 +8,50 @@ const seedAdmin = async () => {
   try {
     await connectDB();
 
+    const adminEmail = process.env.ADMIN_EMAIL;
+
+    const adminPassword = process.env.ADMIN_PASSWORD;
+
+    const adminName = process.env.ADMIN_NAME || "Super Admin";
+
+    const adminPhone = process.env.ADMIN_PHONE;
+
+    if (!adminEmail || !adminPassword || !adminPhone) {
+      throw new Error(
+        "ADMIN_EMAIL, ADMIN_PASSWORD and ADMIN_PHONE are required",
+      );
+    }
+
     const existingAdmin = await User.findOne({
       email: process.env.SEED_ADMIN_EMAIL,
       role: "admin",
     });
 
     if (existingAdmin) {
-      console.log("Admin already exists.");
+      console.log(`Admin already exists: ${adminEmail}`);
       return;
     }
 
-    const hashPassword = await hashedPassword(
+    const password = await hashedPassword(
       process.env.SEED_ADMIN_PASSWORD,
       12,
     );
 
     const admin = await User.create({
-      name: process.env.SEED_ADMIN_NAME,
-      email: process.env.SEED_ADMIN_EMAIL,
-      password: hashPassword,
-      phone:process.env.SEED_ADMIN_PHONE,
+      name: adminName,
+      email: adminEmail,
+      phone: adminPhone,
+      password,
       role: "admin",
     });
 
     console.log(`Admin created successfully : ${admin.email}`);
   } catch (error) {
-    console.error("Admin seeding failed:", error);
+    console.error("Admin seeding failed:", error.message);
     process.exitCode = 1;
   } finally {
     process.exit(0);
   }
 };
-
 
 seedAdmin();
